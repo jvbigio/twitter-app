b09daf5772026a479dd534aba2e44506fdd0a4c0
Joe Bigio <jvbigio@gmail.com>
2020-11-23 21:24:36 -0500

    working on router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './components/Home'
+import Search from './components/Search'
+import Random from './components/Random'
+import Error from './components/Error'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Twitter Clone App</h1>
-    </div>
-  );
+     <BrowserRouter>
+      <div>
+        <Navigation />
+          <Switch>
+            <Route path='/' component={Home} exact/>
+            <Route path="/search" component={Search}/>
+            <Route path="/random" component={Random}/>
+          <Route component={Error}/>
+          </Switch>
+      </div> 
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
