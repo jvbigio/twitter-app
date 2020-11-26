0102709003916ba086602e233c249e32930e6d62
Joe Bigio <jvbigio@gmail.com>
2020-11-26 12:19:01 -0500

    added background image

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 
 function Search () {
   return (
-    <Form className='search-box' autoComplete='off'>
-      <Form.Group controlId='search'>
-        <Form.Control type='text' placeholder='Search Twitter' />
-      </Form.Group>
-    </Form>
+    <div className='container'>
+      {/* test */}
+      <div className='form-container'>
+        <Form inline className='search-box' autoComplete='off'>
+          <Form.Group controlId='search'>
+            <Form.Control type='text' placeholder='Search Twitter' />
+          </Form.Group>
+          <Button id='search-btn' variant='primary' type='submit'>Search</Button>
+        </Form>
+      </div>
+      <div className='content-card'>
+        <h3>Tweet content here</h3>
+      </div>
+    </div>
   )
 }
 
