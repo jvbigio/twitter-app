36ae08552cc0f14847e5ed9c9e7e3632e29d26e5
Joe Bigio <jvbigio@gmail.com>
2020-11-29 13:35:32 -0500

    working on 5 tweet cards

--- src/Pages/Random.js
+++ src/Pages/Random.js
@@ -1,8 +1,77 @@
 import React from 'react'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+import avatar from '../images/popeye.jpg'
 
 function Random () {
   return (
-    <div />
+    <CardDeck>
+      <Card>
+        {/* <Card.Img className='rounded-circle mr-3' variant='top' src={avatar} /> */}
+        <img className='rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Tweet content
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'>Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        {/* <Card.Img className='rounded-circle mr-3' variant='top' src={avatar} /> */}
+        <img className='rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Tweet content
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'>Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        {/* <Card.Img className='rounded-circle mr-3' variant='top' src={avatar} width='40px' height='50px' /> */}
+        <img className='rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Tweet content
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'>Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        {/* <Card.Img className='rounded-circle mr-3' variant='top' src={avatar} /> */}
+        <img className='rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Tweet content
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'>Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        {/* <Card.Img className='rounded-circle mr-3' variant='top' src={avatar} /> */}
+        <img className='rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Tweet content
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className='text-muted'>Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardDeck>
   )
 }
 
