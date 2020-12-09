952a8e74d38d2343df76beadc3508e13b677627d
Joe Bigio <jvbigio@gmail.com>
2020-12-09 18:04:17 -0500

    added dotenv npm pkg for .env file use

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const axios = require('axios')
+require('dotenv').config()
 
 app.use('/', express.static(path.join(__dirname, 'client/build')))
 
 app.listen(3000)
 
-
 // test
 // app.get('/api/tweets', (req, res) => res.send('hello')) // works in postman
 
-// const port = process.env.PORT 
+// const port = process.env.PORT
 // console.log(`Your port is ${port}`)
+// const username = process.env.USERNAME
