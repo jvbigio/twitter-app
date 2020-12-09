09753ef5dbf026e7cb1f9f4f57b0e50cdf25f7a9
Joe Bigio <jvbigio@gmail.com>
2020-12-09 14:30:54 -0500

    added test on server.js. cleaned up css import in search.js

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const app = express()
 app.use('/', express.static(path.join(__dirname, 'client/build')))
 
 app.listen(3000)
+
+
+// test
+app.get('/api/tweets', (req, res) => res.send('hello')) // works in postman
