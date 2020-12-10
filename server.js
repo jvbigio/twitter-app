41243b5ed54f33521bf79d0a1e14ffdb4986ffc5
Joe Bigio <jvbigio@gmail.com>
2020-12-10 12:35:53 -0500

    refactored axios post request

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express()
 const path = require('path')
 const axios = require('axios')
 const qs = require('qs')
-
 require('dotenv').config()
 
 app.use('/', express.static(path.join(__dirname, 'client/build')))
@@ -13,17 +12,38 @@ app.listen(3000)
 
 const url = 'https://api.twitter.com/oauth2/token'
 
-axios.post(url, {
-  qs.stringify({'grant_type':'client_credentials'}),{
+axios.post(url, qs.stringify({ grant_type: 'client_credentials' }), {
   headers: {
-    'Content-Type':'application/x-www-form-urlencoded'
+    'Content-Type': 'application/x-www-form-urlencoded'
   },
+  body: 'grant_type=client_credentials',
   auth: {
     username: process.env.API_KEY,
     password: process.env.API_SECRET_KEY
-  },
+  }
 }).then(response => {
   console.log(response)
 }).catch(error => {
   console.error(error)
 })
+
+// different approach
+// const getBearerToken = async () => {
+//   const data = {
+//     grant_type: 'client_credentials'
+//   }
+//   const headers = {
+//     'content-type': 'application/x-www-form-urlencoded'
+//   }
+//   const url = 'https://api.twitter.com/oauth2/token'
+//   try {
+//     const token = axios.post(url, data)
+//     const tokenParsed = await token
+//     return `${tokenParsed.data.token_type} ${tokenParsed.data.access_token}`
+//   } catch (error) {
+//     console.error(error)
+//     console.log('Token request failed!')
+//     return false
+//   }
+// }
+// getBearerToken()
