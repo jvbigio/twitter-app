0ad4b2e1c4b2df590ad13ed7f916bb7c244730b8
Joe Bigio <jvbigio@gmail.com>
2021-03-07 13:44:11 -0500

    Fixed homepage image and shadow effect

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import './Home.css'
+import Image from 'react-bootstrap/Image'
+import chatBots from '../images/chatBots.png'
 
 function Home () {
   return (
     <div className='home'>
-      {/* <div className='home-card module'>
-        <h1> An app that allows users to read or search tweets, and also generate random ones from your favorite users.</h1>
-      </div> */}
       <div className='container'>
-        <h2>An app that allows users to read or search tweets, and also generate random ones from your favorite users.</h2>
-        <div className='img' id='img' />
+        <h2>An app that allows users to read or search tweets and also generate random ones from your favorite users.</h2>
+        <Image className='shadowed' src={chatBots} fluid />
       </div>
     </div>
   )
