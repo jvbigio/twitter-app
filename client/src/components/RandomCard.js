7b673090e53345d6f3e3f9ee0459df07c814488b
Joe Bigio <jvbigio@gmail.com>
2021-03-29 18:15:01 -0400

    Removed mr-3 on RandomCard img component

--- client/src/components/RandomCard.js
+++ client/src/components/RandomCard.js
@@ -3,13 +3,10 @@ import './RandomCard.css'
 import Card from 'react-bootstrap/Card'
 
 export default function RandomCard (props) {
-  // create random card components
-
   return (
     <div id='card-container'>
-      {/* <Card className='random-card' id='card-container'> */}
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={props.imageSrc} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle' src={props.imageSrc} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
             {props.name}
