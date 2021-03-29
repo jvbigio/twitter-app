f8ea877d9b3ace00bacd1bc6f37194d4c1a0cefc
Joe Bigio <jvbigio@gmail.com>
2021-03-28 20:00:52 -0400

    Added Twitter user profile images to Random component. Removed footer from random card

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -1,75 +1,64 @@
 import React, { useState } from 'react'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
-import avatar from '../images/popeye.jpg'
 import './Random.css'
 import Moment from 'react-moment'
+import andySterks from '../images/andySterks.jpg'
+import bradTraversy from '../images/bradTraversy.jpg'
+import garyV from '../images/garyV.jpg'
+import joeRogan from '../images/joeRogan.jpeg'
+import steveMaxwell from '../images/steveMaxwell.jpeg'
 
 function Random () {
   const [twitterUsers, setTwitterUsers] = useState([
-    { name: 'Steve Maxwell', imageSrc: steveMaxwell },
     { name: 'Andy Sterkowitz', imageSrc: andySterks },
+    { name: 'Brad Traversy', imageSrc: bradTraversy },
     { name: 'Gary Vaynerchuk', imageSrc: garyV },
     { name: 'Joe Rogan', imageSrc: joeRogan },
-    { name: 'Traversy Media', imageSrc: bradTraversy }
+    { name: 'Steve Maxwell', imageSrc: steveMaxwell }
   ])
 
   return (
     <CardDeck id='card-container'>
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle mr-3' src={andySterks} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
-            @Username
+            Andy Sterkowitz
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className='text-muted'>Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle mr-3' src={bradTraversy} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
-            @Username
+            Brad Traversy
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className='text-muted'>Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle mr-3' src={garyV} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
-            @Username
+            Gary Vaynerchuck
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className='text-muted'>Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle mr-3' src={joeRogan} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
-            @Username
+            Joe Rogan
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className='text-muted'>Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
       <Card className='random-card'>
-        <img className='avatar rounded-circle mr-3' src={avatar} alt='avatar' height='50px' width='50px' />
+        <img className='avatar rounded-circle mr-3' src={steveMaxwell} alt='avatar' height='50px' width='50px' />
         <Card.Body>
           <Card.Text>
-            @Username
+            Steve Maxwell
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <small className='text-muted'>Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
     </CardDeck>
   )
