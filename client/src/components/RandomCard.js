989a754dc06316fc69d130b0e09407f54e50db02
Joe Bigio <jvbigio@gmail.com>
2021-03-28 20:52:03 -0400

    Added card-container div to RandomCard

--- client/src/components/RandomCard.js
+++ client/src/components/RandomCard.js
@@ -6,13 +6,15 @@ export default function RandomCard (props) {
   // create random card components
 
   return (
-    <Card className='random-card'>
-      <img className='avatar rounded-circle mr-3' src={props.imageSrc} alt='avatar' height='50px' width='50px' />
-      <Card.Body>
-        <Card.Text>
-          {props.name}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div id='card-container'>
+      <Card className='random-card'>
+        <img className='avatar rounded-circle mr-3' src={props.imageSrc} alt='avatar' height='50px' width='50px' />
+        <Card.Body>
+          <Card.Text>
+            {props.name}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
