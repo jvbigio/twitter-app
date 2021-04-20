3aeaa1f5e6402c0e378a0b67bbb2805005b98b8c
Joe Bigio <jvbigio@gmail.com>
2021-04-20 02:50:05 -0400

    Reduced footer font-size on max 320px width screens

--- client/src/components/TweetCard.js
+++ client/src/components/TweetCard.js
@@ -11,30 +11,39 @@ import Image from 'react-bootstrap/Image'
 import Moment from 'react-moment'
 
 const TweetCard = ({ input, tweet }) => {
-  // const tweetMedia = tweet.entities.media // orig
+  const tweetMedia = tweet.entities.media // orig
   // const tweetMedia = tweet.extended_entities.media
-  const tweetMedia = tweet.extended_entities.media
+  // const tweetMedia = tweet.extended_entities.media
 
   // keep - origin:
-  // const hasMedia = () => tweetMedia ? tweetMedia[0].media_url : null
-  const hasMedia = () => {
-    // console.log(tweetMedia.type)
-    if (tweetMedia) {
-      console.log(tweetMedia[0])
-      // console.log(tweetMedia[0].type) // photo
-    }
-  }
+  const hasMedia = () => tweetMedia ? tweetMedia[0].media_url : null
+  // const hasMedia = () => {
+  //   // console.log(tweetMedia.type)
+  //   if (tweetMedia) {
+  //     console.log(tweetMedia[0])
+  //     // console.log(tweetMedia[0].type) // photo
+  //   }
+  // }
 
   // console.log(tweetMedia[0].type) // works but throws error
   // console.log(tweetMedia.type)
   // console.log(tweet.extended_entities.media[0].type)
 
+  // orig
   const imgStyle = {
     width: 'auto',
     height: 'auto',
     maxWidth: '100%',
     borderRadius: '5%'
   }
+  // const imgStyle = {
+  //   width: 'auto',
+  //   height: '400px',
+  //   minHeight: '400px',
+  //   maxWidth: '100%',
+  //   minWidth: '100%',
+  //   borderRadius: '5%'
+  // }
 
   return (
     <Container fluid>
@@ -74,7 +83,7 @@ const TweetCard = ({ input, tweet }) => {
             </Card.Text>
           </div>
         </Card.Body>
-        <Card.Footer id='footer-icons' className='d-flex justify-content-around rounded-bottom'>
+        <Card.Footer className='d-flex justify-content-around rounded-bottom footer-icons'>
           <div id='comment'><FaRegComment /> &nbsp; {tweet.display_text_range[1]} </div>
           <div id='retweet'><FaRetweet /> &nbsp; {tweet.retweet_count}</div>
           <div id='heart'><FaRegHeart /> &nbsp; {tweet.favorite_count}</div>
